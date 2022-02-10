17bb5654e9ca3788fa99526af8d85e7e148dbc4e
Jessica Majors <jessicalomajors@gmail.com>
2022-02-10 15:47:06 -0500

    Added variables and method skeletons

---
+++ tmpClasses/Guest.java
@@ -0,0 +1,30 @@
+/**
+*
+* Represents a guest user of the app (no account)
+*
+* @author Nicholas Fang, Nicholas Gorki, Ron Kovler, Ryan Lechner, Jessica Majors
+* @version {date}
+* 
+**/
+
+public class Guest {
+
+	public Guest() {}
+
+    /*
+    * Class methods
+    */
+
+    /* Show timeline based on most liked posts in last 24 hrs */
+    public void getHotTimeline() {
+    }
+
+    /* Show timemline based on followed users and topics */
+    public void getPersonalTimeline() {
+    }
+
+    /* show selected user profile */
+    public void getUserProfile(User user) {
+    }
+	
+}
