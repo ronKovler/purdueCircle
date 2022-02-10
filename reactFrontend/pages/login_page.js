802d648c2adafb7bb1fe5a96c26e3e31826cf446
Karueo <nicobgw25@gmail.com>
2022-02-10 11:42:51 -0500

    created seperate stylesheet and create account pg

--- reactFrontend/pages/login_page.js
+++ reactFrontend/pages/login_page.js
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, TextInput, View, Button, Image, Alert } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, TextInput, View, Button, Image, Alert } from 'react-native';
 import React, { useState } from 'react';
+import { styles } from './stylesheet';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
@@ -21,59 +20,12 @@ export default function LoginScreen({ navigation }) {
         <View style={styles.button}>
           <Button title="Login" onPress={() => Alert.alert("bad")}/>
         </View>
+        <View style={styles.button}>
+          <Button title="Create Account" onPress={navigation.navigate("Create Account")}/>
+        </View>
       </View>
     </View>
   );
 }
 
 //TODO: Adjust sizing of page to resize to display size? do we want to fix for mobile rn?
-const styles = StyleSheet.create({
-  container: {
-    resizeMode: 'contain',
-    flex: 1,
-    backgroundColor: '#737373',
-    alignItems: 'center',
-    justifyContent: 'center',
-    //transform: [{scale: 1.25}],
-  },
-
-  loginBox: {
-    resizeMode: 'contain',
-    backgroundColor: '#545454',
-    justifyContent: 'center',
-    alignContent: 'center',
-    padding: 10,
-  },
-
-  inputBox: {
-    backgroundColor: '#d9d9d9',
-    textDecorationColor: '#a6a6a6',
-    justifyContent: 'center',
-    padding: 7.5,
-    margin: 5,
-    minWidth: 200,
-  },
-
-  image: {
-    flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'contain',
-    aspectRatio: 1,
-    //margin: 15,
-    maxWidth: 968,
-  },
-
-  button:{
-    margin: 5,
-  },
-
-  header: {
-    textAlign: 'center',
-    color: '#ffc000',
-    fontWeight: 'bold',
-    margin: 5,
-    fontSize: 18,
-  },
-}
-)
