6e1559df4db1efeee93f49dbdf8b482461fd0aed
Karueo <nicobgw25@gmail.com>
2022-02-17 17:52:59 -0500

    fixed fucky-wucky

--- reactFrontend/pages/home_page.js
+++ reactFrontend/pages/home_page.js
@@ -1,21 +1,17 @@
 import React, {Component, useState} from 'react';
 import {View, Text, Button, StatusBar, StyleSheet, Pressable} from 'react-native';
 import {styles} from './stylesheet';
+import {TouchableOpacity} from "react-native-web";
 // TODO Post UI and import to timeline?
 
 export default function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-
       <Text style={styles.header}>Welcome to Purdue Circle!</Text>
       <Post/>
       <View style={styles.buttonContainer}>
-        <View style={styles.button}>
-          <Button title="Login" onPress={() => navigation.navigate('Login')}/>
-        </View>
-        <View style={styles.button}>
-          <Button title="Create Account" onPress={() => navigation.navigate('Create Account')}/>
-        </View>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}><Text>Login</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Create Account')}><Text>Create Account</Text></TouchableOpacity>
       </View>
       <StatusBar style="auto"/>
     </View>
