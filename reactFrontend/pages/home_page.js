202027ab13e67ac741db62ec611784376cd8c948
ryanlechner215 <ryanlechner215@yahoo.com>
2022-02-19 01:45:39 -0500

    Fixed ScrollView on main page and added flex margins left/right

--- reactFrontend/pages/home_page.js
+++ reactFrontend/pages/home_page.js
@@ -1,27 +1,54 @@
 import React, {} from 'react';
-import {View, Text } from 'react-native';
-import {styles, HeaderLogo} from './stylesheet';
-import {ScrollView, TouchableOpacity} from "react-native-web";
+import { View, Text, StyleSheet } from 'react-native';
+import { styles, HeaderLogo } from './stylesheet';
+import { ScrollView, TouchableOpacity } from "react-native-web";
 import Post from "./post";
 import User from "./user";
 
 export default function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <HeaderLogo style={styles.headerIcon}/>
-        {!User.isLoggedIn ?
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}><Text
-              style={styles.button}>Login</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Create Account')}><Text
-              style={styles.button}>Register</Text></TouchableOpacity>
-          </View> : null}
+    <View style={styled.container}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
+            </View>
+              <View style={{flex: 2, flexDirection: 'row', alignSelf: 'center', justifyContent: 'center'}}>
+                <HeaderLogo style={styles.headerIcon}/>
+                {!User.isLoggedIn ?
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}><Text
+                      style={styles.button}>Login</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Create Account')}><Text
+                      style={styles.button}>Register</Text></TouchableOpacity>
+                  </View> : null}
+              </View>
+            <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
+            </View>
       </View>
-      <ScrollView style={{flex: 10}}>
-        <Post/>
-        <Post/><Post/><Post/><Post/><Post/>
-      </ScrollView>
+      <View style={{flex: 10, flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
+        </View>
+        <View style={{flex: 2, flexDirection: 'column'}}>
+            <View style={{flex: 1, backgroundColor: '737373'}}/>
+            <ScrollView style={{flex: 100, flexBasis: 10}}>
+                <Post/><Post/><Post/><Post/><Post/><Post/>
+                <Post/><Post/><Post/><Post/><Post/><Post/>
+            </ScrollView>
+            <View style={{flex: 1, backgroundColor: '737373'}}/>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styled = StyleSheet.create({
+    container: {
+        resizeMode: 'contain',
+        flex: 1,
+        backgroundColor: '#737373',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        minHeight: 'fit-content',
+      }
+})
