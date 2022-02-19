7ffbfccf6dc978bc93063c5d71dd56506abf2ef4
Nicholas Fang <nlfang@purdue.edu>
2022-02-19 12:59:01 -0500

    Added email/username validation errors

--- reactFrontend/pages/create_account.js
+++ reactFrontend/pages/create_account.js
@@ -9,6 +9,8 @@ export default function CreateAccountScreen({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [reenter, setReenter] = useState('')
+  const [usernameError, setUsernameError] = useState('')
+  const [emailError, setEmailError] = useState('')
 
 
   //TODO: insert URL
@@ -24,6 +26,31 @@ export default function CreateAccountScreen({navigation}) {
     })
   }
 
+  const validateSubmission = () => {
+    var validUsername = false
+    var validEmail = false
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (username.length == 0) {
+      setUsernameError("Username is required")
+    } else {
+      setUsernameError("")
+      validUsername = true
+    }
+
+    if (email.length == 0) {
+      setEmailError("Email is required")
+    } else if (reg.test(email) == false) {
+      setEmailError("Enter a valid email address")
+    } else {
+      setEmailError("")
+      validEmail = true
+    }
+
+    if (validUsername && validEmail) {
+      Register()
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.loginBox}>
@@ -35,9 +62,11 @@ export default function CreateAccountScreen({navigation}) {
                    placeholder="Last Name"/>
         <TextInput style={styles.accountInputBox}
                    placeholder="Username"/>
+        {usernameError.length > 0 && <Text style={{color: 'red'}}>{usernameError}</Text>}
         <TextInput style={styles.accountInputBox}
                    placeholder='Email'
                    onChangeText={email => setEmail(email)}/>
+        {emailError.length > 0 && <Text style={{color: 'red'}}>{emailError}</Text>}
         <TextInput style={styles.accountInputBox}
                    placeholder='Password'
                    secureTextEntry={true}
@@ -46,7 +75,7 @@ export default function CreateAccountScreen({navigation}) {
                    placeholder="Re-enter Password"
                    secureTextEntry={true} onChangeText={reenter => setReenter(reenter)}/>
         {password !== reenter ? <Text style={{color:'red'}}>Passwords do not match</Text> : null}
-        <TouchableOpacity onPress={() => Alert.alert("do nothing")}><Text style={styles.button}>Register</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => validateSubmission()}><Text style={styles.button}>Register</Text></TouchableOpacity>
       </View>
     </View>
   )
