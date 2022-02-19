8380ec5be44156688d44fd6cfe62e55b61d539f4
ryanlechner215 <ryanlechner215@yahoo.com>
2022-02-19 02:31:09 -0500

    In a very cool turn of events, the comments have been removed because comments break react stuff

--- reactFrontend/pages/home_page.js
+++ reactFrontend/pages/home_page.js
@@ -9,11 +9,11 @@ export default function HomeScreen({navigation}) {
 
     return (
         <View style={styled.container}>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}> // Upper section of the page, with the logo and buttons
-                <View style={{flex: 1, backgroundColor: 'dimgrey'}}/>                       // Left margin of the upper section
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flex: 1, backgroundColor: 'dimgrey'}}/>
                 <View style={{flex: 2, flexDirection: 'row', alignSelf: 'center', justifyContent: 'center'}}>
-                    <HeaderLogo style={styles.headerIcon}/>                  // Headerlogo displays no matter what
-                    {!User.isLoggedIn ?                                      // Display login/signup buttons depending on whether logged in or not
+                    <HeaderLogo style={styles.headerIcon}/>
+                    {!User.isLoggedIn ?
                         <View style={styles.buttonContainer}>
                         <TouchableOpacity onPress={() => navigation.navigate('Login')}><Text
                             style={styles.button}>Login</Text></TouchableOpacity>
@@ -21,20 +21,20 @@ export default function HomeScreen({navigation}) {
                             style={styles.button}>Register</Text></TouchableOpacity>
                     </View> : null}
                 </View>
-                <View style={{flex: 1, backgroundColor: 'dimgrey'}}/>           // Right margin of the upper section
+                <View style={{flex: 1, backgroundColor: 'dimgrey'}}/>
             </View>
-            <View style={{flex: 10, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '737373'}}>  // This begins the lower section of the page
-                <View style={{flex: 1, backgroundColor: 'dimgrey'}}>                         // Left margin of the lower section
+            <View style={{flex: 10, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '737373'}}>
+                <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
                 </View>
-                <View style={{flex: 2, flexDirection: 'column'}}>                            // Container for scrollview, to give a fixed height
-                    <View style={{flex: 1, backgroundColor: '737373'}}/>                     // Nesting scrollview in view with flexbasis fixed the problem
-                    <ScrollView style={{flex: 100, flexBasis: 100}}>                         // Of the scrollview extending way off the page
-                        <Post/><Post/><Post/><Post/><Post/><Post/>                           // Scrollview and contents
+                <View style={{flex: 2, flexDirection: 'column'}}>
+                    <View style={{flex: 1, backgroundColor: '737373'}}/>
+                    <ScrollView style={{flex: 100, flexBasis: 100}}>
+                        <Post/><Post/><Post/><Post/><Post/><Post/>
                         <Post/><Post/><Post/><Post/><Post/><Post/>
                     </ScrollView>
-                    <View style={{flex: 2, backgroundColor: '737373'}}/>                     // Margin at the bottom, technically could have been a margin prop
+                    <View style={{flex: 2, backgroundColor: '737373'}}/>
                 </View>
-                <View style={{flex: 1, backgroundColor: 'dimgrey'}}>                         // Right margin of the lower section
+                <View style={{flex: 1, backgroundColor: 'dimgrey'}}>
                 </View>
             </View>
         </View>
@@ -45,8 +45,8 @@ const styled = StyleSheet.create({
     container: {
         resizeMode: 'contain',
         flex: 1,
-        backgroundColor: '#737373',                   // Slightly altered version of container from the style sheet
-        alignItems: 'stretch',                        // Main difference is that this is a stretch align
+        backgroundColor: '#737373',
+        alignItems: 'stretch',
         justifyContent: 'center',
         minHeight: 'fit-content',
       }
