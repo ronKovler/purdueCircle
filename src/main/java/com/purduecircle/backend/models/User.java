62a2d2fc7e5cf7a1109d235bc6d5f34d8cfe6934
Ron Kovler <kovler.ron@gmail.com>
2022-02-20 03:28:27 -0500

    Samething previously pt2, also added cmd script for building and deploying jar executable to server

--- src/main/java/com/purduecircle/backend/models/User.java
+++ src/main/java/com/purduecircle/backend/models/User.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 
 @Entity
 @Table(name = "users")
-
 public class User {
 
     /*
@@ -51,6 +50,19 @@ public class User {
 
     @Column(name="phone_number", nullable=true)
     private String phone_number;
+
+    public User(String first_name, String last_name, String email, String username, String password, String phone_number) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phone_number = phone_number;
+    }
+
+    public User() {
+
+    }
 
     /*
     * Class methods
