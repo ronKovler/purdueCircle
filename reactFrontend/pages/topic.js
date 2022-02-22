9e6b636ac6d00de4faca482b50af7ea7e867e1ba
ryanlechner215 <ryanlechner215@yahoo.com>
2022-02-22 17:23:18 -0500

    adding topic.js for displaying followed topics

---
+++ reactFrontend/pages/topic.js
@@ -0,0 +1,13 @@
+import {Pressable, StyleSheet, Text, View, Image} from "react-native";
+import React, {useState} from "react";
+import styles from './stylesheet';
+
+export default function Post(props, {navigation}) {
+  const [topic, setTopic] = useState('topic')
+
+  return (
+    <View style={{backgroundColor: "#5F5F5F", margin: 5, flexDirection: 'column', height: 'fit-content', borderRadius: 20}}>
+
+    </View>
+    )
+}
