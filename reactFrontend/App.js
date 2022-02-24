847d1aee0be71d3bea1cb52e545732c01087593b
Ron Kovler <kovler.ron@gmail.com>
2022-02-24 01:06:05 -0500

    Original server working again, chaanged back to it

--- reactFrontend/App.js
+++ reactFrontend/App.js
@@ -9,7 +9,7 @@ import HomeScreen from './pages/home_page';
 import EditProfile from './pages/edit_profile';
 import {Text} from "react-native";
 
-global.serverAddress = "http://ec2-3-140-216-16.us-east-2.compute.amazonaws.com:443";
+global.serverAddress = "http://3.137.16.13:443";
 
 const Stack = createNativeStackNavigator();
 
